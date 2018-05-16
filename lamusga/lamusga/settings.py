f3e271ff976470af68e43fbe40468b47a9e63362
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-05-15 23:51:41 -0400

    App spotifywrapper added

--- lamusga/lamusga/settings.py
+++ lamusga/lamusga/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'lastfmwrapper',
+    'spotifywrapper',
 ]
 
 MIDDLEWARE = [
---
+++ lamusga/spotifywrapper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpotifywrapperConfig(AppConfig):
+    name = 'spotifywrapper'
