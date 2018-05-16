40b7251f82b6d4b3bff9d1751d755eb780e932bc
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-05-16 00:09:49 -0400

    Configuring vcs to filter lastfm api_key and versioning the cassette

--- lamusga/lastfmwrapper/tests/test_user.py
+++ lamusga/lastfmwrapper/tests/test_user.py
@@ -1,9 +1,17 @@
-import vcr
+import os
+
+import vcr as vcr_module
+from django.conf import settings
 
 from lastfmwrapper import User
 
 
-@vcr.use_cassette('vcr_cassettes/user-recent-tracks.yml')
+vcr = vcr_module.VCR(filter_query_parameters=['api_key'])
+cassettes_path = os.path.join(settings.BASE_DIR,
+                              'lastfmwrapper/tests/vcr_cassettes/{}')
+
+
+@vcr.use_cassette(cassettes_path.format('user-recent-tracks.yml'))
 def test_recent_tracks():
     username = 'diegodukao'
     lfm_user = User(username)
