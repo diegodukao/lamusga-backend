613915ce585a1d779ed7f6a5669d67d8016594ea
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-05-21 14:43:20 -0300

    lastfm: recent_tracks test ensuring call is made with the correct params

--- lamusga/lastfmwrapper/tests/test_user.py
+++ lamusga/lastfmwrapper/tests/test_user.py
@@ -1,6 +1,10 @@
 import os
 
+from unittest.mock import call, patch
+
+import requests
 import vcr as vcr_module
+
 from django.conf import settings
 
 from lastfmwrapper import User
@@ -15,7 +19,16 @@ cassettes_path = os.path.join(settings.BASE_DIR,
 def test_recent_tracks():
     username = 'diegodukao'
     lfm_user = User(username)
-    response = lfm_user.recent_tracks()
+
+    requests_to_mock_path = 'lastfmwrapper.user.requests.get'
+    with patch(requests_to_mock_path, wraps=requests.get) as mock_get:
+        response = lfm_user.recent_tracks()
+
+        expected_call = call(
+            f'{lfm_user.server}?method=user.getrecenttracks&user={username}',
+            params=lfm_user.request_params
+        )
+        assert expected_call in mock_get.call_args_list
 
     assert isinstance(response, dict)
     assert set(['track', '@attr']) == response.keys()
